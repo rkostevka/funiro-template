ecd46245b3f7c445477b201cdfe2d862c4a15238
ruslana <ruslana.voznyuk@gmail.com>
2021-09-17 15:04:04 +0300

    burger menu

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -1,6 +1,6 @@
 
 window.onload = function () {
-	document.addEventListener("click", documentActions)
+	document.addEventListener("click", documentActions);
 
 	function documentActions(e) {
 		const targetElement = e.target;
@@ -17,11 +17,14 @@ window.onload = function () {
 					"_hover"
 				);
 			}
-			if (targetElement.classList.contains('search-form__icon')) {
-				document.querySelector('.search-form').classList.toggle('_active');
-			} else if(!targetElement.closest('.search-form') && document.querySelector('.search-form._active')) {
-				document.querySelector('.search-form').classList.remove('_active');
-			}
+		}
+		if (targetElement.classList.contains("search-form__icon")) {
+			document.querySelector(".search-form").classList.toggle("_active");
+		} else if (
+			!targetElement.closest(".search-form") &&
+			document.querySelector(".search-form._active")
+		) {
+			document.querySelector(".search-form").classList.remove("_active");
 		}
 	}
 }
