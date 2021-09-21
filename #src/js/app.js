c30edf25f6883052be76ee4f7e9a233b1447650f
ruslana <ruslana.voznyuk@gmail.com>
2021-09-21 20:27:13 +0300

    slider

--- #src/js/app.js
+++ #src/js/app.js
@@ -1,4 +1,5 @@
-@@include('files/regular.js', {})
+@@include('files/dynamic_adapt.js', {})
+@@include('files/sliders.js', {})
 @@include('files/functions.js', {})
 @@include('files/script.js', {})
 @@include('files/forms.js', {})
--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -27,4 +27,17 @@ window.onload = function () {
 			document.querySelector(".search-form").classList.remove("_active");
 		}
 	}
+
+	//HEADER
+	const callback = function(entries, observer) {
+		if(entries[0].isIntersecting) {
+			headerElement.classList.remove('_scroll');
+		} else {
+			headerElement.classList.add('_scroll');
+		}
+	}
+	const headerElement = document.querySelector('.header');
+	const headerObserver = new IntersectionObserver(callback);
+	headerObserver.observe(headerElement);
+
 }
