c389ba868948bbceca51ea9924a50223637a95a8
ruslana <ruslana.voznyuk@gmail.com>
2021-10-11 14:56:21 +0300

    tips slider

--- #src/js/files/sliders.js
+++ #src/js/files/sliders.js
@@ -108,3 +108,42 @@ if (document.querySelector(".slider-rooms__body")) {
 		},
 	});
 }
+
+if (document.querySelector(".slider-tips__body")) {
+	new Swiper(".slider-tips__body", {
+		observer: true,
+		observeParents: true,
+		slidesPerView: 3,
+		spaceBetween: 32,
+		speed: 800,
+		loop: true,
+		watchOverflow: true,
+		// Dotts
+		pagination: {
+			el: ".slider-tips__dotts",
+			clickable: true,
+		},
+		// Arrows
+		navigation: {
+			nextEl: ".slider-tips .slider-arrow_next",
+			prevEl: ".slider-tips .slider-arrow_prev",
+		},
+		breakpoints: {
+			// when window width is >= 320px
+			320: {
+				slidesPerView: 1.1,
+				spaceBetween: 15,
+			},
+			// when window width is >= 768px
+			768: {
+				slidesPerView: 2,
+				spaceBetween: 20,
+			},
+			// when window width is >= 992px
+			992: {
+				slidesPerView: 3,
+				spaceBetween: 32,
+			},
+		},
+	});
+}
